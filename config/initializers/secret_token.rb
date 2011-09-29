c84d199bc4af817371efad39d18e23d29db57f81
Fabio Perrella <fabio.perrella@gmail.com>
2011-09-29 08:02:57 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locamotiva::Application.config.secret_token = 'ec14f2aaa29465168b9239f948cf9b5ed82d64f897678c6c4a114bd5faa689a19040158767641157d3f387c1a26494b518db085becc4722da428a3abb066cfec'
