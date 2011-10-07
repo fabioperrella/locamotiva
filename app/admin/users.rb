940bb5734807aab2b4c2289fab9e27da07e48847
Fabio Perrella <fabio.perrella@gmail.com>
2011-10-07 09:54:33 -0300

    add campos do user no form de cadastro

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,8 +1,20 @@
 ActiveAdmin.register User do
   index do
-  	column :name
-  	column :email  	
-  	column :shirt_size
-  	column :department  	
+    column :name
+    column :email
+    column :shirt_size
+    column :department
+  end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :email
+      f.input :shirt_size, :as => :select, :collection => ["P", "M", "G"]
+      f.input :department
+      f.input :cpf
+      f.input :born_at
+    end
+    f.buttons
   end
 end
