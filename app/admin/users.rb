7776e3dcd5658ac5a45cb915d688e833bdffba8d
Fabio Perrella <fabio.perrella@gmail.com>
2011-10-10 20:28:59 -0300

    fazendo pagina de cadastro de usuarios sign_up

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -21,10 +21,10 @@ ActiveAdmin.register User do
     f.inputs "Details" do
       f.input :name
       f.input :email
-      f.input :shirt_size, :as => :select, :collection => ["Baby Look","P", "M", "G", "GG"]
+      f.input :shirt_size, :as => :select, :collection => AppConfig.shirt_sizes
       f.input :department
       f.input :cpf
-      f.input :birth_date
+      f.input :birth_date, :start_year => 1960
       f.input :role, :as => :select, :collection => ["corredor","admin"], :selected => "corredor", :include_blank => false
     end
     f.buttons
