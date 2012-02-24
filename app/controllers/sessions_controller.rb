07e6ac2e8463d3b6af2768f7d63ccbf9ca403f3c
Fabio Perrella <fabio.perrella@gmail.com>
2012-02-24 10:05:47 -0200

    atualizando views do devise

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < Devise::SessionsController
-  def create
+  # GET /resource/sign_in
+  def new    
+    resource = build_resource
+    clean_up_passwords(resource)
+    # respond_with(resource, serialize_options(resource))
+    render 'devise/sessions/new'
+  end
+
+  # POST /resource/sign_in
+  # def create
+  #   resource = warden.authenticate!(auth_options)
+  #   set_flash_message(:notice, :signed_in) if is_navigational_format?
+  #   sign_in(resource_name, resource)
+  #   respond_with resource, :location => after_sign_in_path_for(resource)
+  # end
+
+  def create    
     resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
     set_flash_message(:notice, :signed_in) if is_navigational_format?
     sign_in(resource_name, resource)
