7038c453326b3dab9b3750267496a2df8919eac9
Fabio Perrella <fabio.perrella@gmail.com>
2012-03-05 23:07:28 -0300

    acao de votar ok

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,4 +7,12 @@ class VotesController < ApplicationController
     end
     @races = Race.where(date: @date_filter.beginning_of_month..@date_filter.end_of_month).order(:date)
   end
+
+  def create
+    vote = Vote.new race_id: params["race_id"], user: current_user
+    if vote.save
+      flash[:notice] = I18n.t(:vote_created_sucessfull)
+    end
+    redirect_to votes_path
+  end
 end
--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe VotesController do
+  before :each do
+    @user = FactoryGirl.create :user
+    sign_in :user, @user
+  end
+
   describe "GET index" do
     context "when not passing month and year params" do
       it "assigns races with races that will happen next month ordered by date" do
@@ -30,6 +35,29 @@ describe VotesController do
     it "renders index template" do
       get :index
       response.should render_template("index")
+    end
+  end
+
+  describe "POST create" do
+    before :each do
+      @race = create :race
+    end
+
+    it "creates a vote for the logged user in a race" do
+      post :create, {race_id: @race.id}
+      vote = Vote.last
+      vote.race.should == @race
+      vote.user.should == @user
+    end
+
+    it "renders index layout" do
+      post :create, {race_id: @race.id}
+      response.should redirect_to(votes_path)
+    end
+
+    it "flashes notice sucessfull" do
+      post :create, {race_id: @race.id}
+      flash[:notice].should == I18n.t(:vote_created_sucessfull)
     end
   end
 end
